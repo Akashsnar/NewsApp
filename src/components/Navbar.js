9e969e8024cd0078c3fa20164df2ba2bc511719d
Akashsnar <aksn0204@gmail.com>
2024-12-23 18:02:05 +0530

    Removed title not defined error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,7 +72,7 @@ const Navbar=(props)=>{
                 </nav>
 
 
-    {/* <div className="container" id="searchcomponent">
+   {/* <div className="container" id="searchcomponent">
                          
                     <div className="row">
                         {articles && articles.map((element) => {
@@ -91,92 +91,3 @@ const Navbar=(props)=>{
 
 }
 export default Navbar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, {useState} from 'react';
-// import {  Link } from "react-router-dom";
-
-// const Navbar=(props)=>{
-     
-//     const [articles, setArticles] = useState([])
-//     const [loading, setLoading] = useState(true)
-//     // const [page, setPage] = useState(1)
-//     // const [totalResults, setTotalResults] = useState(0)
-    
-//     const searchcountry= async(event)=>{
-//         let searchquery=event.target.value;
-//       console.log(searchquery);
-//        props.searchfunc(searchquery);
-// }
-// const setcategory= async(event)=>{
-// console.log(event);
-// }
-
-
-
-
-//     return (
-//             <div>
-
-//                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//                     <div className="container-fluid">
-//                         <Link className="navbar-brand"to="/">NewsApp</Link>
-//                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                             <span className="navbar-toggler-icon"></span>
-//                         </button>
-//                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//                                 <li className="nav-item">
-//                                     <Link className="nav-link active" aria-current="page"to="/">Home</Link>
-//                                 </li>
-//                                 <li className="nav-item btn" onClick={() =>setcategory("Business")} >    Business</li>
-//                                 {/* <li className="nav-item"><Link className="nav-link"to="/Entertainment"   >Entertainment</Link></li>
-//                                 <li className="nav-item"><Link className="nav-link"to="/General"         >General</Link></li>
-//                                 <li className="nav-item"><Link className="nav-link"to="/Health"          >Health</Link></li>
-//                                 <li className="nav-item"><Link className="nav-link"to="/Science"         >Science</Link></li>
-//                                 <li className="nav-item"><Link className="nav-link"to="/Sports"          >Sports</Link></li>
-//                                 <li className="nav-item"><Link className="nav-link"to="/Technology"      >Technology</Link></li> */}
-//                                  <li className="nav-item btn" onClick={() =>setcategory("Entertainment")} >Entertainment</li>
-//                                 <li className="nav-item btn" onClick={() =>setcategory("General")}         >General</li>
-//                                 <li className="nav-item btn" onClick={() =>setcategory("Health")}          >Health</li>
-//                                 <li className="nav-item btn" onClick={() =>setcategory("Science")}         >Science</li>
-//                                 <li className="nav-item btn" onClick={() =>setcategory("Sports")}        >Sports</li>
-//                                 <li className="nav-item btn" onClick={() =>setcategory("Technology")}      >Technology</li> 
-
-
-//                             </ul>
-//                             <form action="/somewhere_else" className="d-flex" role="search" >
-//                                 <input className="form-control me-2" onChange={(e)=> searchcountry(e)} type="search" placeholder="Search" aria-label="Search"/>
-//                                     <button className="btn btn-outline-success" type="submit">Search</button>
-//                             </form>
-//                         </div>
-//                     </div>
-//                 </nav>
-
-
-
-//             </div>
-//         )
-
-// }
-// export default Navbar;
-
-
--- src/components/Newsbody.js
+++ src/components/Newsbody.js
@@ -1,148 +1,4 @@
-// import React, { useEffect, useState} from 'react'
-// import NewsItem from './NewsItem';
-// import Spinner from './Spinner';
-// import PropTypes from 'prop-types'
-// import InfiniteScroll from "react-infinite-scroll-component";
-
-
-
-// const Newsbody=(props)=>{
-// const [articles, setarticles] = useState([]);
-// // const [loading, //setloading] = useState(false);
-// const [totalResults, settotalResults] = useState(false);
-// const [page, setpage] = useState(1);
-// document.title="NewsApp"
-
-  
-//     // constructor(){
-//     //     super();
-//     //     ={
-//     //         articles:[],
-//     //         loading: false,
-//     //         page: 1,
-//     //         totalResults: 0
-//     //     }
-//     //     document.title="NewsApp"
-//     // }
-    
-
-//     // async componentDidMount(){
-// //     let url='https://newsapi.org/v2/everything?q=tesla&from=20230718&sortBy=publishedAt&category=${props.category}&apiKey=1d5e90a8e7af41218104e811f0e981a0';
-// //     let data= await fetch(url);
-// //     let parsedata = await data.json();
-// //     console.log(parsedata);
-// // }
-
-// const updatenews= async ()=>{
-//   props.setProgress(0);
-//   let url=`https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.api_key}&page=${page}&pagesize=${props.pagesize}&q=${props.query}&q=${props.query}`
-//   // //setloading(true);
-
-//     let data= await fetch(url)
-//   props.setProgress(30);
-
-//     let parsedata=await data.json()
-//   props.setProgress(60);
-
-//   // //setloading(false);
-//   setarticles(parsedata.articles);
-//   settotalResults(parsedata.totalResults);
-
-
-//   props.setProgress(100);
-
-// }
-
-
-// useEffect( () => {
-//   updatenews();
-// }, [])
-
-
-
-// // previouspage = async ()=>{
-
-// // setState({
-// //   page: .page-1, 
-// // })
-// // updatenews();
-// // }
-
-// // nextpage= async ()=>{
-// // setState({
-// //   page: .page+1, 
-// // })
-// // updatenews();
-// // }
-
-
-// const fetchMoreData = async()=>{
-//   setpage(page+1);
-  
-//   let url=`https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.api_key}&page=${page}&pagesize=${props.pagesize}&q=${props.query}&q=${props.query}`
-//   //setloading(true);
-//     let data= await fetch(url)
-//     let parsedata=await data.json()
-//     // setState({articles: .articles.concat(parsedata.articles), totalResults:parsedata.totalResults, loading:false})
-    
-//   //setloading(false);
-//   setarticles(articles.concat(parsedata.articles));
-//   settotalResults(parsedata.totalResults);
-// }
-
-
-//     return (
-//      <>
-//         {/* {loading && <Spinner/>} */}
-
-
-
-//         <InfiniteScroll
-//           dataLength={articles.length}
-//           next={fetchMoreData}
-//           hasMore={articles.length!==totalResults}
-//           loader={<Spinner/> }
-//         >
-//            <div className="container my-3">
-//            <div className="row">
-//   {/* {!.loading && .articles.map((element)=>{ */}
-//   {articles.map((element)=>{
-// return <div className="col-md-4" key={element.url}>
-//       <NewsItem title={element.title?element.title: ""} description={element.description?element.description.slice(0, 95)+"....":""} imageurl={element.urlToImage?element.urlToImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeaifaI7Xt6w7_u1MWfeuRSUXpPNkb586Ocw&usqp=CAU"} key={element.url} 
-//       newsurl={element.url}  author={element.author?element.author:"unknown"} date={element.publishedAt}  source={element.source.name} />
-//     </div>
-    
-//   })} 
-// </div>
-// </div>
-// </InfiniteScroll>
-
-
-// {/* <div className='container d-flex justify-content-between'>
-// <button disabled={.page<=1} onClick={previouspage} type="button" className="btn btn-dark">&laquo; Previous</button>
-// <button disabled={.page+1 > Math.ceil(.totalarticle/props.pagesize)} type="button" onClick={nextpage} className="btn btn-dark">Next &raquo;</button>
-// </div> */}
-//       </>
-//     )
-//   }
-
-
-// Newsbody.defaultProps={
-//   country :'in',
-//   pagesize: 20,
-//   category: 'general'
-// }
-
-// Newsbody.propTypes = {
-//   country: PropTypes.string,
-//   pagesize: PropTypes.number,
-//   category: PropTypes.string,
-// }
-
-// export default Newsbody;
-
-///////////////////////////////////////
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
@@ -152,30 +8,29 @@ import InfiniteScroll from "react-infinite-scroll-component";
 //     useLocation
 //   } from "react-router-dom";
 
-const News = (props)=>{
+const News = (props) => {
     const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
     const [totalResults, setTotalResults] = useState(0)
-   
+
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
-    } 
+    }
 
-    const updateNews = async ()=> {
+    const updateNews = async () => {
         props.setProgress(10);
-        let url='';
-        if(props.newstype==='everything' && props.query){
-        // url =`https://newsapi.org/v2/everything?&q=${props.query}&apiKey=${props.api_key}`
-        console.log(props.query);
-        url=`https://gnews.io/api/v4/search?q=${props.query}&lang=en&apikey=${props.api_key}`
-        // url=  `https://newsapi.org/v2/everything?q=${props.query}&apiKey=1d5e90a8e7af41218104e811f0e981a0&language=en`
+        let url = '';
+        if (props.newstype === 'everything' && props.query) {
+            console.log(props.query);
+            url = `https://gnews.io/api/v4/search?q=${props.query}&lang=en&apikey=${props.api_key}`
+            // url=  `https://newsapi.org/v2/everything?q=${props.query}&apiKey=1d5e90a8e7af41218104e811f0e981a0&language=en`
         }
-        else if(props.category){
+        else if (props.category) {
             console.log(props.category);
-            url=`https://gnews.io/api/v4/top-headlines?category=${props.category}&lang=en&apikey=${props.api_key}`
+            url = `https://gnews.io/api/v4/top-headlines?category=${props.category}&lang=en&apikey=${props.api_key}`
 
-        // url =`https://newsapi.org/v2/${props.newstype}?country=in&category=${props.category}&apiKey=${props.api_key}&page=${page}&pagesize=${props.pagesize}&q=${props.query}`
+            // url =`https://newsapi.org/v2/${props.newstype}?country=in&category=${props.category}&apiKey=${props.api_key}&page=${page}&pagesize=${props.pagesize}&q=${props.query}`
         }
         setLoading(true)
         let data = await fetch(url);
@@ -194,57 +49,74 @@ const News = (props)=>{
 
     useEffect(() => {
         document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
-        updateNews(); 
+        updateNews();
         console.log("why2");
         // eslint-disable-next-line
     }, [])
 
 
-    const fetchMoreData = async () => {   
-        let url='';
-        if(props.newstype==='everything' && props.query){
-        console.log(props.query);
-        url=`https://gnews.io/api/v4/search?q=${props.query}&lang=en&apikey=${props.api_key}`
-        // url=  `https://newsapi.org/v2/everything?q=${props.query}&apiKey=1d5e90a8e7af41218104e811f0e981a0&language=en`
+    const fetchMoreData = async () => {
+        try {
+            let url = '';
+            if (props.newstype === 'everything' && props.query) {
+                console.log(props.query);
+                url = `https://gnews.io/api/v4/search?q=${props.query}&lang=en&apikey=${props.api_key}`
+                // url=  `https://newsapi.org/v2/everything?q=${props.query}&apiKey=1d5e90a8e7af41218104e811f0e981a0&language=en`
+            }
+            else if (props.category) {
+                url = `https://gnews.io/api/v4/top-headlines?category=${props.category}&lang=en&apikey=${props.api_key}`
+                // url =`https://newsapi.org/v2/${props.newstype}?country=in&category=${props.category}&apiKey=${props.api_key}&page=${page}&pagesize=${props.pagesize}&q=${props.query}`
+            }
+            setPage(page + 1)
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            if (parsedData.articles) {
+                setArticles((prevArticles) => [...prevArticles, ...parsedData.articles]);
+            } else {
+                console.error("No articles found in API response");
+            }
+            setTotalResults(parsedData.totalArticles || 0);
+            console.log(totalResults);
+            console.log(articles.length);
         }
-        else if(props.category){
-            url=`https://gnews.io/api/v4/top-headlines?category=${props.category}&lang=en&apikey=${props.api_key}`
-        // url =`https://newsapi.org/v2/${props.newstype}?country=in&category=${props.category}&apiKey=${props.api_key}&page=${page}&pagesize=${props.pagesize}&q=${props.query}`
+        catch (error) {
+            console.error("Error fetching data:", error);
         }
-        setPage(page+1) 
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        setArticles(articles.concat(parsedData.articles))
-        setTotalResults(parsedData.totalArticles)
-        console.log(totalResults);
-        console.log(articles.length);
 
-      };
- 
-        return (
-            <>
-                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
-                {loading && <Spinner />}
-                <InfiniteScroll
-                    dataLength={articles.length}
-                    next={fetchMoreData}
-                    hasMore={articles.length < totalResults-10}
-                    loader={<Spinner/>}
-                > 
-                    <div className="container">
-                         
+    };
+
+    return (
+        <>
+            <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+            {loading && <Spinner />}
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                hasMore={articles.length < totalResults - 10}
+                loader={<Spinner />}
+                
+            >
+                <div className="container">
+
                     <div className="row">
                         {articles?.map((element) => {
+                            if (!element || !element.title || !element.description || !element.url) {
+                                return null;
+                            }
                             return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""}  imageUrl={element.image} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""}
+                                    imageUrl={element.image} newsUrl={element.url}
+                                    author={element.author ? element.author : "Unknown"}
+                                    date={element.publishedAt ? element.publishedAt : "Not available"}
+                                    source={element.source.name ? element.source.name : "Unknown"} />
                             </div>
                         })}
                     </div>
-                    </div> 
-                </InfiniteScroll>
-            </>
-        )
-    
+                </div>
+            </InfiniteScroll>
+        </>
+    )
+
 }
 
 
@@ -259,9 +131,4 @@ News.propTypes = {
 }
 
 export default News
-
-
 //https://gnews.io/api/v4/top-headlines?category=General&lang=en&country=us&max=10&apiKey=f9fff24bda681513a1e6d8cd95dceb3c
-//https://gnews.io/api/v4/top-headlines?category=General&lang=en&country=us&max=10&apiKey=f9fff24bda681513a1e6d8cd95dceb3c
-
-//https://gnews.io/api/v4/top-headlines?category=General&lang=en&country=us&max=10&apikey=f9fff24bda681513a1e6d8cd95dceb3c
